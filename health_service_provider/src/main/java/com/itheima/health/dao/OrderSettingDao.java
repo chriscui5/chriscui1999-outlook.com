34eedf89cd87964f17bd41655868b33c268619ae
chriscui <chriscui1999@outlook.com>
2020-04-01 00:06:57 +0800

    day 5

---
+++ health_service_provider/src/main/java/com/itheima/health/dao/OrderSettingDao.java
@@ -0,0 +1,39 @@
+package com.itheima.health.dao;
+
+import com.itheima.health.pojo.OrderSetting;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author ：seanyang
+ * @date ：Created in 2019/6/11
+ * @description ：
+ * @version: 1.0
+ */
+public interface OrderSettingDao {
+	/**
+	 * 添加预约设置
+	 * @param orderSetting 预约设置数据
+	 */
+	void add(OrderSetting orderSetting);
+
+	/**
+	 * 基于预约日期更新预约设置
+	 * @param orderSetting 预约设置数据
+	 */
+	void updateOrderSettingByOrderDate(OrderSetting orderSetting);
+
+	/**
+	 * 统计某一日期下的数据
+	 * @param date
+	 * @return
+	 */
+	Long countByOrderDate(Date date);
+
+	/**
+	 * 获取某月数据
+	 * @param date 提供月初与月末时间段
+	 */
+	List<OrderSetting> getOrderSettingByMonth(Map date);
+}
